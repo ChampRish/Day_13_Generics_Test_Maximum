8a2a583d161f11dd08dd693a6fdabf5bb173132a
RISHABH <gguptarishabh82@gmail.com>
2022-03-16 02:26:30 +0530

    Given 3 Floats find the maximum

---
+++ UC2maxFloatNum.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace UC2maxFloatNum
+{
+    class Program
+    {
+        public static double maxFloatNumber(double fNum, double sNum, double tNum)
+        {
+            if (fNum.CompareTo(sNum) > 0 && fNum.CompareTo(tNum) > 0 ||
+                fNum.CompareTo(sNum) >= 0 && fNum.CompareTo(tNum) > 0 ||
+                fNum.CompareTo(sNum) > 0 && fNum.CompareTo(tNum) >= 0)
+            {
+                return fNum;
+            }
+            if (sNum.CompareTo(fNum) > 0 && sNum.CompareTo(tNum) > 0 ||
+                sNum.CompareTo(fNum) >= 0 && sNum.CompareTo(tNum) > 0 ||
+                sNum.CompareTo(fNum) > 0 && sNum.CompareTo(tNum) >= 0)
+            {
+                return sNum;
+            }
+            if (tNum.CompareTo(fNum) > 0 && tNum.CompareTo(sNum) > 0 ||
+                tNum.CompareTo(fNum) >= 0 && tNum.CompareTo(sNum) > 0 ||
+                tNum.CompareTo(fNum) > 0 && tNum.CompareTo(sNum) >= 0)
+            {
+                return tNum;
+            }
+            return fNum;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Greater Float Number is : " + Program.maxFloatNumber(1.5, 10.4, 7.4));
+        }
+    }
+}
